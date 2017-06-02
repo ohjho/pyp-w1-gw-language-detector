21093f96169c258ce6df64a0208f649ad4c8e146
John Ho <jho.yvr+c9@gmail.com>
2017-06-02 01:29:23 +0000

    This is our final solution.

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
+from languages import LANGUAGES
 
 """This is the entry point of the program."""
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    
+    count = {}
+    for language in languages:
+        matches = (len(set(text.split(' ')) & set(language['common_words'])))
+        count[language['name']] = matches
+    
+    return max(count, key=count.get)
+        
+    
+
